692329c83438a9579f18ea34262980ddc46629d0
Khalil <khalilsantana92@gmail.com>
2025-10-01 21:44:30 -0300

    publicarbranch

---
+++ src/controllers/exercicios.js
@@ -0,0 +1,47 @@
+import ServiceExercicio from "../services/exercicios.js"
+
+class ControllerExercicio {
+
+
+     Exercicio1Get (req, res){
+        const num1 = Number(req.query.num1)
+        const num2 = Number(req.query.num2)
+        const result = ServiceExercicio.somar(num1, num2)
+        res.status(200).send({ num1, num2, result })
+    }
+
+     Exercicio1Post (req, res){
+        try {
+            const num1 = req.body.num1
+            const num2 = req.body.num2
+            const result = ServiceExercicio.somar(num1, num2)
+            res.status(200).send({
+                msg: "O resultado é " + result,
+                data: result,
+                error: null
+            })
+        } catch (error) {
+            res.status(400).send({
+                msg: error.message,
+                data: null,
+                error: true
+            })
+        }
+
+
+    }
+
+     exercicio2(req, res) {
+        // const valorHora = req.params.valorHora
+        // const qtdeHoras = req.params.qtdeHoras
+        const { valorHora, qtdeHoras } = req.params
+
+        const result = ServiceExercicio.salario(valorHora, qtdeHoras)
+
+        res.status(200).send({
+            result
+        })
+    }
+}
+
+export default new ControllerExercicio()
